a5b3adb40da35def32ca0f5942bb9b11566fa520
Tarapynya, Pavel <pavel.tarapynya@dxc.com>
2023-12-09 00:47:42 +0100

    day7_p2

--- day7/main.cpp
+++ day7/main.cpp
@@ -7,6 +7,7 @@ unordered_map<char, uint> card;
 unordered_map<char, uint> getMapOfCard()
 {
     unordered_map<char, uint> card;
+    card['J'] = 1;
     card['2'] = 2;
     card['3'] = 3;
     card['4'] = 4;
@@ -16,7 +17,6 @@ unordered_map<char, uint> getMapOfCard()
     card['8'] = 8;
     card['9'] = 9;
     card['T'] = 10;
-    card['J'] = 11;
     card['Q'] = 12;
     card['K'] = 13;
     card['A'] = 14;
@@ -68,26 +68,55 @@ auto getCardFrequencyMap(const unordered_map<char, int>& frequency)
     }
     return orderedHand;
 }
+
+int isJaks(const string& hand)
+{
+    return count_if(begin(hand), end(hand), [](auto& card){
+        return card == 'J';
+    });
+}
+
 Combinations sortHands(string& cards)
 {
     unordered_map<char, int> cardsInHand = getCardFrequency(cards);
     auto maxCombination = maxSameCardsInHand(cardsInHand);
-    // cout << " " << maxCombination.second << " ";
+
+    int j = isJaks(cards);
     Combinations combinations;
     switch(maxCombination.second)
     {
         case 1:
             combinations = Combinations::kiker; 
+            if(j == 1)
+            {
+                combinations = Combinations::pair;
+            }
             break;
         case 2:
             cardsInHand.erase(maxCombination.first);
             if(maxSameCardsInHand(cardsInHand).second == 2)
             {
                 combinations = Combinations::twoPairs;
+                if(j == 1)
+                {
+                    combinations = Combinations::fullHouse;
+                }
+                if(j == 2)
+                {
+                    combinations = Combinations::four;
+                }
             }
             else
             {
                 combinations = Combinations::pair;
+                if(j == 2)
+                {
+                    combinations = Combinations::set;
+                }
+                if(j == 1)
+                {
+                    combinations = Combinations::set;
+                }
             }
             break;
         case 3:
@@ -95,46 +124,62 @@ Combinations sortHands(string& cards)
             if(maxSameCardsInHand(cardsInHand).second == 2)
             {
                 combinations = Combinations::fullHouse;
+                if(j == 3)
+                {
+                    combinations = Combinations::five;
+                }
+                if(j == 2)
+                {
+                    combinations = Combinations::five;
+                }
             }
             else
             {
                 combinations = Combinations::set;
+                if(j == 1)
+                {
+                    combinations = Combinations::four;
+                }
+                if(j == 3)
+                {
+                    combinations = Combinations::four;
+                }
             }
             break;
         case 4:
             combinations = Combinations::four;
+            if(j == 1)
+            {
+                combinations = Combinations::five;
+            }
+            if(j == 4)
+            {
+                combinations = Combinations::five;
+            }
             break;
         case 5:
             combinations = Combinations::five;
             break;
     }
-    // cout << cards << " " << static_cast<std::underlying_type<Combinations>::type>(combinations) << endl;
+
     return combinations;
 }
 auto sortCombination(string& hand1)
 {
     const auto& cardFrequency1 = getCardFrequency(hand1);
     const auto& frequencyMap1 = getCardFrequencyMap(cardFrequency1);
-    // cout << endl;
-    // cout << hand1 << " " << endl;
     int k = 0;
     for(const auto& elem : frequencyMap1)
     {
-        // cout << elem.first.first << " " << elem.second << " "; 
-
         for(int i = 0; i < elem.first.first; i++)
         {
-            // cout << elem.first.first << " " << elem.second << " "; 
             hand1[k] = elem.first.second;
             k++;
-            // cout << " " << newHand << " ";
         }
     }
-    // cout << newHand << " s" << endl;
-    // cout << endl;
     return ;
 }
-auto comparePair(const string& hand1, const string& hand2)
+auto compareHandsJaks(const string& hand1, const string& hand2)
 {
     const auto& cardFrequency1 = getCardFrequency(hand1);
     const auto& cardFrequency2 = getCardFrequency(hand2); 
@@ -145,8 +190,6 @@ auto comparePair(const string& hand1, const string& hand2)
 
     while(card1It != frequencyMap1.end() && card2It != frequencyMap2.end())
     {
-        // cout << card1It->first.first << " it " << card1It->second << endl; 
-        // cout << card2It->first.first << " it " << card2It->second << endl; 
         if(card1It->second == card2It->second)
         {
             card1It++;
@@ -161,7 +204,6 @@ auto comparePair(const string& hand1, const string& hand2)
     return false;
 }
 
-// auto compareCombinations(, )
 
 int main()
 {
@@ -173,7 +215,6 @@ int main()
     auto comp = [](const auto& hand1, const auto& hand2){
        if(hand1.second == hand2.second)
        {
-            // return !comparePair(hand1.first, hand2.first);
             for(int i = 0; hand1.first.size(); i++)    
             {
                 if(hand1.first[i] != hand2.first[i])
@@ -193,14 +234,10 @@ int main()
     {
         auto index = line.find(' ');
         auto pair = make_pair<string, int>(line.substr(0, index), stoi(line.substr(index + 1)));
-
         // sort(pair.first.begin(), pair.first.end(), compareHighCards);
         // sortCombination(pair.first);
         auto piarHandCombination = make_pair<string, int>(move(pair.first),  static_cast<int>(sortHands(pair.first)));
         auto res = sortingHands.insert(std::make_pair(piarHandCombination, pair.second));
-        // if(res.second)
-            // cout << "insetrt ";
-        // cout << piarHandCombination.first << " " << piarHandCombination.second << endl;
     }
     long i = 1;
     long res = 0;
@@ -208,6 +245,7 @@ int main()
     {
         res += i * elem.second;
         i++;
+        if(isJaks(elem.first.first))
         cout << elem.first.first << " " << elem.first.second << " " << elem.second << endl;
     }
     cout << res << " " << i - 1 << endl;
